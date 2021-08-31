327903a5dcbf1c6632caa48af108d0341bc51b40
Antônio Oliveira <antonio.filho128@gmail.com>
2021-08-31 19:03:47 -0300

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,5 @@
-key = '[[YOUTUBE-API-KEY]]'
+youtube_api_key = '[[YOUTUBE-API-KEY]]'
 playlist_id = '[[PLAYLIST-ID]]'
+
+google_drive_client_id = '[[GOOGLE-DRIVE-CLIENT-ID]]'
+google_drive_public_folder_id = '[[GOOGLE-DRIVE-PUBLIC-FOLDER-ID]]'
---
+++ remove.py
@@ -0,0 +1,7 @@
+"""
+- Obtém lista de vídeos da playlist do YouTube
+- Obtém lista de arquivos do Google Drive, confronta com lista do YouTube e verifica se algum arquivo deve ser removido.
+- Deleta do Google Drive os arquivos obtidos na verificação
+- Obtém a lista de arquivos do Google Drive, desta vez atualizada
+- Atualiza, commita e sobe arquivo backing-tracks.json atualizado
+"""
