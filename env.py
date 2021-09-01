ae3844fdac257c636f97d053cef8e81e9cca6394
Antônio Oliveira <antonio.filho128@gmail.com>
2021-09-01 19:12:24 -0300

    Update env.py

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-youtube_api_key = os.environ['YOUTUBE_API_KEY']
-playlist_id = os.environ['PLAYLIST_ID']
-google_drive_client_id = os.environ['GOOGLE_DRIVE_CLIENT_ID']
-google_drive_public_folder_id = os.environ['GOOGLE_DRIVE_PUBLIC_FOLDER_ID']
+youtube_api_key = os.environ.get('YOUTUBE_API_KEY')
+playlist_id = os.environ.get('PLAYLIST_ID')
+google_drive_client_id = os.environ.get('GOOGLE_DRIVE_CLIENT_ID')
+google_drive_public_folder_id = os.environ.get('GOOGLE_DRIVE_PUBLIC_FOLDER_ID')
