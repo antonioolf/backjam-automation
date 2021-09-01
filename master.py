ea7a1d586dbbb21c5ba816222543cf31771eef77
Antônio Oliveira <antonio.filho128@gmail.com>
2021-09-01 19:44:45 -0300

    Finishes test

--- master.py
+++ master.py
@@ -2,14 +2,8 @@ import download
 import publish
 import remove
 import upload
-from env import google_drive_public_folder_id, playlist_id
 
 if __name__ == '__main__':
-    print(f'>>>>>>>>>> {google_drive_public_folder_id}')
-    print(f'>>>>>>>>>> {playlist_id}')
-
-    exit(0)
-
     download.run()
     upload.run()
     publish.run()
