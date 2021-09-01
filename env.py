c442f5f610c55fccce34490b375b108d1c0f4cbc
Antônio Oliveira <antonio.filho128@gmail.com>
2021-09-01 18:55:40 -0300

    Melhoria no env.py

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
-youtube_api_key = '[[YOUTUBE-API-KEY]]'
-playlist_id = '[[PLAYLIST-ID]]'
+import os
 
-google_drive_client_id = '[[GOOGLE-DRIVE-CLIENT-ID]]'
-google_drive_public_folder_id = '[[GOOGLE-DRIVE-PUBLIC-FOLDER-ID]]'
+youtube_api_key = os.environ['YOUTUBE_API_KEY']
+playlist_id = os.environ['PLAYLIST_ID']
+google_drive_client_id = os.environ['GOOGLE_DRIVE_CLIENT_ID']
+google_drive_public_folder_id = os.environ['GOOGLE_DRIVE_PUBLIC_FOLDER_ID']
